e9dd6ed48b8668693dd1c2be21c8ae5cffac647b
admin <admin@admins-MBP.attlocal.net>
2021-05-22 10:33:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// 10:26 is basically where we left off. 
+
+import React, { useEffect } from 'react'; // fetches API data everytime app loads. 
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=327e5fcdbbcdb402ff611d8debb36868&format=1';
 
 function App() {
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data.rates))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1>Money Enjoyer</h1>
+     <CurrencyRow />
+     <div className="equals"> = </div>
+     <CurrencyRow />
+    </>
   );
 }
 
---
+++ src/CurrencyRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CurrencyRow() {
+    return (
+        <div>
+            <input type="number" className="input" />
+            <select>
+                <option value="hello">hello</option>
+            </select>
+        </div>
+    )
+}
+
+export default CurrencyRow
